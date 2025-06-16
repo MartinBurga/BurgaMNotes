68443cb204c120ca864560e90c71482af7dc3644
MartinBurga <mburga.xixv@gmail.com>
2025-06-15 19:42:02 -0500

    Adicion de MainPage y solucion de errores

---
+++ BurgaMNotes/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace BurgaMNotes.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
